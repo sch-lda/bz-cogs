581e5e77baa3540ca59f2325116454832e342a8b
cheng <admin@schplus.com>
2024-09-07 18:36:46 +0800

    fix merge

--- aiuser/messages_list/messages.py
+++ aiuser/messages_list/messages.py
@@ -69,7 +69,8 @@ class MessagesList:
 
     async def _init(self, prompt=None):
         self.model = await self.config.guild(self.guild).model()
-        self.token_limit = await self.config.guild(self.guild).custom_model_tokens_limit() or self._get_token_limit(self.model)        try:
+        self.token_limit = await self.config.guild(self.guild).custom_model_tokens_limit() or self._get_token_limit(self.model)
+        try:
             self._encoding = tiktoken.encoding_for_model(self.model)
         except KeyError:
             self._encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")

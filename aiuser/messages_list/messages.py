eb1bd7e8929e6f21b4044f39a01e7f6776b0f123
cheng <admin@schplus.com>
2024-09-07 18:34:39 +0800

    merge 7dbd022d58f3153242a27b603142a5c97668e23f

--- aiuser/messages_list/messages.py
+++ aiuser/messages_list/messages.py
@@ -69,8 +69,7 @@ class MessagesList:
 
     async def _init(self, prompt=None):
         self.model = await self.config.guild(self.guild).model()
-        self.token_limit = self._get_token_limit(self.model)
-        try:
+        self.token_limit = await self.config.guild(self.guild).custom_model_tokens_limit() or self._get_token_limit(self.model)        try:
             self._encoding = tiktoken.encoding_for_model(self.model)
         except KeyError:
             self._encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
@@ -277,6 +276,8 @@ class MessagesList:
             limit = 31000
         if "100k" in model or "claude" in model:
             limit = 99000
+        if "llama-3.1" in model:
+            limit = 123000
         model = model.split("/")[-1]
         if model in OTHER_MODELS_LIMITS:
             limit = OTHER_MODELS_LIMITS.get(model, limit)
